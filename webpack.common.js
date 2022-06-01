5d4398dba560ac768db5046d9ffd00a63eb178f8
razi16 <99330297+razi16@users.noreply.github.com>
2022-06-01 16:10:20 +0700

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+ 
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            /* style and css loader */
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    /* plugin */
+    plugins: [
+        /* HTML Webpack Plugin */
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            filename: "index.html"
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/scores.html",
+            filename: "scores.html"
+        })
+    ]
+}
